b10d18e56b7f69f6f6a2afef8ceca5e33c71b8a5
pr0kuk <alex-shch@yandex.ru>
2022-10-07 02:01:49 +0300

    added simd

--- matrix.cpp
+++ matrix.cpp
@@ -1,5 +1,9 @@
+#define THREAD_NUM 12
+//#define pragmapar for simd
+#define pragmapar for
 #include <vector>
 #include <iostream>
+#include <omp.h>
 class Matrix {
     public:
     std::vector<std::vector<int>> data;
@@ -28,6 +32,7 @@ std::vector<int> operator + (std::vector<int> v1, std::vector<int> v2) {
     if (v1.size() != v2.size() || v1.size() == 0)
         std::cout << "sizes noe equal or == 0" << std::endl;
     else
+    #pragma omp parallel pragmapar
         for(int i = 0; i < v1.size(); i++)
             v3[i] = v1[i] + v2[i];
     return v3;
@@ -38,6 +43,7 @@ std::vector<int> operator - (std::vector<int> v1, std::vector<int> v2) {
     if (v1.size() != v2.size() || v1.size() == 0)
         std::cout << "sizes noe equal or == 0" << std::endl;
     else
+    #pragma omp parallel pragmapar
         for(int i = 0; i < v1.size(); i++)
             v3[i] = v1[i] - v2[i];
     return v3;
@@ -48,6 +54,7 @@ std::vector<std::vector<int>> operator + (std::vector<std::vector<int>> v1, std:
     if (v1.size() != v2.size() || v1.size() == 0)
         std::cout << "sizes noe equal or == 0" << std::endl;
     else
+    #pragma omp parallel pragmapar
         for(int i = 0; i < v1.size(); i++)
             v3[i] = v1[i] + v2[i];
     return v3;
@@ -58,6 +65,7 @@ std::vector<std::vector<int>> operator - (std::vector<std::vector<int>> v1, std:
     if (v1.size() != v2.size() || v1.size() == 0)
         std::cout << "sizes noe equal or == 0" << std::endl;
     else
+    #pragma omp parallel pragmapar
         for(int i = 0; i < v1.size(); i++)
             v3[i] = v1[i] - v2[i];
     return v3;
@@ -81,12 +89,11 @@ void Matrix::print() {
 
 Matrix operator * (Matrix t1, Matrix t2) {
     std::vector<std::vector<int>> tdata(t1.data.size(), std::vector<int>(t2.data[0].size()));
-    for (int x = 0; x < t1.data.size(); ++x)
-      for (int z = 0, sum = 0; z < t2.data[0].size(); tdata[x][z++] = sum, sum = 0) {
-        for (int y = 0; y < t1.data[0].size(); ++y)
-          sum += t1.data[x][y] * t2.data[y][z];
-        //std::cout << sum << std::endl;
-      }
+    #pragma omp parallel pragmapar collapse(3)
+    for (int x = 0; x < t1.data.size(); x++)
+      for (int z = 0; z < t2.data[0].size(); z++)
+        for (int y = 0; y < t1.data[0].size(); y++)
+          tdata[x][z] += t1.data[x][y] * t2.data[y][z];
     return Matrix(tdata);
 }
 
@@ -101,18 +108,22 @@ std::vector<std::vector<Matrix>> separating(Matrix& t) {
     int X = t.data.size(), Y = t.data[0].size();
     int X2 = log2(X), Y2 = log2(Y);
     std::vector<std::vector<Matrix>> ret(2, std::vector<Matrix>(2, Matrix(X2, Y2)));
-      for (int i = 0; i < X2; ++i)
+    #pragma omp parallel pragmapar collapse(2)
+    for (int i = 0; i < X2; ++i)
         for (int j = 0; j < Y2; ++j)
-          ret[0][0].data[i][j] = t.data[i][j];
-      for (int i = 0; i < X2; ++i)
+            ret[0][0].data[i][j] = t.data[i][j];
+    #pragma omp parallel pragmapar collapse(2) 
+    for (int i = 0; i < X2; ++i)
         for (int j = Y2; j < Y; ++j)
-          ret[0][1].data[i][j - Y2] = t.data[i][j];
-      for (int i = X2; i < X; ++i)
+            ret[0][1].data[i][j - Y2] = t.data[i][j];
+    #pragma omp parallel pragmapar collapse(2) 
+    for (int i = X2; i < X; ++i)
         for (int j = 0; j < Y2; ++j)
-          ret[1][0].data[i - X2][j] = t.data[i][j];
-      for (int i = X2; i < X; ++i)
+            ret[1][0].data[i - X2][j] = t.data[i][j];
+    #pragma omp parallel pragmapar collapse(2) 
+    for (int i = X2; i < X; ++i)
         for (int j = Y2; j < Y; ++j)
-          ret[1][1].data[i - X2][j - Y2] = t.data[i][j];
+            ret[1][1].data[i - X2][j - Y2] = t.data[i][j];
     return ret;
 }
 
@@ -121,15 +132,19 @@ Matrix unite(std::vector<std::vector<Matrix>>& AB) {
     int Y = AB[0][0].data.size() + AB[1][0].data.size();
     int X2 = log2(X), Y2 = log2(Y);
     Matrix ret(X, Y);
+    #pragma omp parallel pragmapar collapse(2) 
       for (int i = 0; i < X2; i++)
         for (int j = 0; j < Y2; j++)
           ret.data[i][j]= AB[0][0].data[i][j];
+    #pragma omp parallel pragmapar collapse(2) 
       for (int i = 0; i < X2; i++)
         for (int j = Y2; j < Y; j++)
           ret.data[i][j] = AB[0][1].data[i][j - Y2];
+    #pragma omp parallel pragmapar collapse(2) 
       for (int i = X2; i < X; i++)
         for (int j = 0; j < Y2; j++)
           ret.data[i][j] = AB[1][0].data[i - X2][j];
+    #pragma omp parallel pragmapar collapse(2) 
       for (int i = X2; i < X; i++)
         for (int j = Y2; j < Y; j++)
           ret.data[i][j] = AB[1][1].data[i - X2][j - Y2];
@@ -152,12 +167,36 @@ Matrix strassen(Matrix& t1, Matrix& t2) {
     return unite(AB);
 }
 
-int main() {
-    Matrix t1({{1, 1}, {1,1}, {10, 10}});
-    Matrix t2({{5,7,2},{3,3,4}});
+Matrix square_rand_matrix(int siz) {
+    Matrix ret(siz, siz);
+    for (int i = 0; i < siz; i++)
+        for (int j = 0; j < siz; j++)
+            ret.data[i][j] = rand() % 1024;
+    return ret;
+}
+
+int main(int argc, char* argv[]) {
+    int nthr = 1;
+    int siz = 16;
+    if (argc >= 2)
+        nthr = atoi(argv[1]);
+    if (argc > 2)
+        siz = atoi(argv[2]);
+    omp_set_num_threads(nthr);
+    Matrix t1 = square_rand_matrix(siz), t2 = square_rand_matrix(siz);
+    double start = omp_get_wtime(), end = 0;
     Matrix a2 = t1 * t2;
-    a2.print();
-    std::cout << std::endl;
+    std::cout << omp_get_wtime() - start << std::endl;
+    //std::cout << std::endl;
+    start = omp_get_wtime(), end = 0;
     Matrix a3 = strassen(t1, t2);
-    a3.print();
+    std::cout << omp_get_wtime() - start << std::endl;
+    // a2.print();
+    // std::cout << std::endl;
+    // std::cout << std::endl;
+    // a3.print();
+    if (a2 == a3)
+        std::cout << "correct" << std::endl;
+    else
+        std::cout << "incorrect" << std::endl; 
 }
---
+++ script.bat
@@ -0,0 +1,15 @@
+@REM a.exe 1 1024
+@REM a.exe 2 1024
+@REM a.exe 3 1024
+@REM a.exe 4 1024
+@REM a.exe 5 1024
+@REM a.exe 6 1024
+@REM a.exe 7 1024
+@REM a.exe 8 1024
+@REM a.exe 9 1024
+@REM a.exe 10 1024
+@REM a.exe 11 1024
+@REM a.exe 12 1024
+a.exe 1 512
+a.exe 1 1024
+@REM a.exe 1 2048
