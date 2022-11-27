6252663839fb768bb726180730412639f3576598
pr0kuk <alex-shch@yandex.ru>
2022-11-28 01:06:29 +0300

    fixed idents

--- lab/openmp.cpp
+++ lab/openmp.cpp
@@ -8,7 +8,7 @@
     fprintf(d, "%d %f\n", num, time);\
     for (int i = 0; i < ISIZE; ++i)\
         for (int j = 0; j < JSIZE; ++j)\
-            ASSERT_DOUBLE_EQ(a[i][j], b[i][j]);}while(0)
+            ASSERT_DOUBLE_EQ(a[i][j], b[i][j]);} while(0)
 
 #include <math.h>
 #include <omp.h>
@@ -16,22 +16,17 @@
 #include <stdio.h>
 #include "gtest/gtest.h"
 
-
-
 double a[ISIZE][JSIZE];
 double b[ISIZE][JSIZE];
 
 
-double parallel(int nthr, double m[ISIZE][JSIZE] )
+double parallel(int nthr, double m[ISIZE][JSIZE])
 {
-    int i, j;
     double start = omp_get_wtime();
-    // #pragma omp parallel for num_threads(nthr) collapse(2) schedule(static, 1)
-    for (i=0; i<ISIZE-4; i++){
+    for (int i = 0; i < ISIZE-4; i++) {
         #pragma omp parallel for num_threads(nthr)
-        for (j = 5; j < JSIZE; j++){
-            m[i][j] = sin(0.1*b[i+4][j-5]);
-        }
+        for (int j = 5; j < JSIZE; j++)
+            m[i][j] = sin(0.1 * b[i + 4][j - 5]);
     }
     double time = omp_get_wtime() - start;
     std::cout << nthr << " " << time << std::endl;
@@ -40,40 +35,31 @@ double parallel(int nthr, double m[ISIZE][JSIZE] )
 
 double sequential(double m[ISIZE][JSIZE] )
 {
-    int i, j;
     double start = omp_get_wtime();
-    for (i=0; i<ISIZE-4; i++){
-        for (j = 5; j < JSIZE; j++){
-            m[i][j] = sin(0.1*m[i+4][j-5]);
-        }
+    for (int i = 0; i < ISIZE - 4; i++) {
+        for (int j = 5; j < JSIZE; j++)
+            m[i][j] = sin(0.1 * m[i + 4][j - 5]);
     }
     double time = omp_get_wtime() - start;
-
     std::cout << "1 " << time << std::endl;
     return time;
 }
 
-int print_m(const char* const s, double m[ISIZE][JSIZE])
+void print_m(const char* const s, double m[ISIZE][JSIZE])
 {
     FILE* ff = fopen(s, "w");
-    for(int i=0; i < ISIZE; i++){
-        for (int j=0; j < JSIZE; j++){
-            fprintf(ff,"%f ",m[i][j]);
-        }
-    fprintf(ff,"\n");
+    for(int i = 0; i < ISIZE; i++){
+        for (int j=  0; j < JSIZE; j++)
+            fprintf(ff, "%f ", m[i][j]);
+        fprintf(ff, "\n");
     }
     fclose(ff);
-    return 0;
 }
 
-int init_m(double m[ISIZE][JSIZE] ) {
-    int i = 0, j = 0;
-    for (i=0; i<ISIZE; i++){
-        for (j=0; j<JSIZE; j++){
+void init_m(double m[ISIZE][JSIZE] ) {
+    for (int i = 0; i < ISIZE; i++)
+        for (int j=0; j < JSIZE; j++)
             m[i][j] = 10*i +j;
-        }
-    }
-    return 0;
 }
 
 
