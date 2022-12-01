5535e6c3b3f57a1bbe016369286c0fd526e91368
pr0kuk <alex-shch@yandex.ru>
2022-12-01 23:44:37 +0300

    added hello task

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main(int argc, char *argv[]) {
+    #pragma omp parallel 
+    {
+        std::cout << omp_get_thread_num() << " Hello world!" << std::endl;
+    }
+    return 0;
+}
